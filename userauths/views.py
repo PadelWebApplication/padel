6279a7949515c368124b63786f78544cdd5f57a3
kllimenkoo <kliiimenko.al.123654@gmail.com>
2026-03-19 16:05:43 +0200

    Login setup

--- userauths/views.py
+++ userauths/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 
 from userauths import forms as userauths_forms
+from userauths import models as userauths_models
 from coach import models as coach_models
 from client import models as client_models
 
@@ -12,30 +13,74 @@ def register_view(request):
         messages.success(request, "You are already logged in.")
         return redirect("/")
     
-    form = userauths_forms.UserRegisterForm(request.POST or None)
+    if request.method == "POST":
+        form = userauths_forms.UserRegisterForm(request.POST or None)
     
+        if form.is_valid():
+            user = form.save()
+            full_name = form.cleaned_data.get("full_name")
+            email = form.cleaned_data.get("email")
+            password1 = form.cleaned_data.get("password1")
+            user_type = form.cleaned_data.get("user_type")
 
+            user = authenticate(request, email=email, password=password1)
 
-    if form.is_valid():
-        user = form.save()
-        full_name = form.cleaned_data.get("full_name")
-        email = form.cleaned_data.get("email")
-        password = form.cleaned_data.get("password")
-        user_type = form.cleaned_data.get("user_type")
+            if user is not None:
+                login(request, user)
 
-        # user_ = authenticate(email=email, password=password)
-        # login(request, user_)
+                if user_type == "Coach":
+                    coach_models.Coach.objects.create(user=user, full_name=full_name)
+                else:
+                    client_models.Client.objects.create(user=user, full_name=full_name, email=email)
 
-        if user_type == "Coach":
-            coach_models.Coach.objects.create(user=user, full_name=full_name)
-        else:
-            client_models.Client.objects.create(user=user, full_name=full_name, email=email)
-
-        messages.success(request, "Account created successfully.")
-        return redirect("/")
+                messages.success(request, "Account created successfully.")
+                return redirect("/")
+            else:
+                messages.error(request, "Authentication failed, please try again.")
+    else:
+        form = userauths_forms.UserRegisterForm()
 
     context = {
         "form": form
     }
 
     return render(request, "userauths/sign-up.html", context)
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        messages.success(request, "You are already logged in.")
+        return redirect("/")
+    
+    if request.method == "POST":
+        form = userauths_forms.LoginForm(request.POST or None)
+        
+        if form.is_valid():
+            email = form.cleaned_data.get("email") 
+            password = form.cleaned_data.get("password") 
+
+            try:
+                user_instance = userauths_models.User.objects.get(email=email, is_active=True)
+                user_authenticate = authenticate(request, email=email, password=password)
+
+                if user_instance is not None:
+                    login(request, user_authenticate)
+                    
+                    messages.success(request, "Account created successfully.")
+                    
+                    next_url = request.GET.get("next", "/")
+                    return redirect(next_url)
+                else:
+                    messages.error(request, "Username or password does not exist.")
+            except:
+                messages.error(request, "User does not exist.")
+    else:
+        form = userauths_forms.LoginForm()
+
+    context = {
+        "form": form
+    }
+
+    return render(request, "userauths/sign-in.html", context)
+
+
