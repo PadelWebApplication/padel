9bfea42b868ae8e84032ea57d7165fcd5f3035f0
kllimenkoo <kliiimenko.al.123654@gmail.com>
2026-03-31 11:31:49 +0300

    added session list and detail, SessionNote, ActionItem, Resource models to the base

--- coach/urls.py
+++ coach/urls.py
@@ -5,5 +5,7 @@ from coach import views
 app_name = "coach"
 
 urlpatterns = [
-     path("", views.dashboard, name="dashboard")
+     path("", views.dashboard, name="dashboard"),
+     path("sessions/", views.sessions, name="sessions"),
+     path("session_detail/<session_id>/", views.session_detail, name="session_detail"),
 ]
--- coach/views.py
+++ coach/views.py
@@ -16,3 +16,35 @@ def dashboard(request):
     }
 
     return render(request, "coach/dashboard.html", context)
+
+
+@login_required
+def sessions(request):
+    coach = coach_models.Coach.objects.get(user=request.user)
+    sessions = base_models.Session.objects.filter(coach=coach)
+
+    context = {
+        "sessions": sessions,
+    }
+
+    return render(request, "coach/sessions.html", context)
+
+
+@login_required
+def session_detail(request, session_id):
+    coach = coach_models.Coach.objects.get(user=request.user)
+    session = base_models.Session.objects.get(session_id=session_id, coach=coach)
+
+    session_notes = base_models.SessionNote.objects.filter(session=session)
+    action_items = base_models.ActionItem.objects.filter(session=session)
+    resources = base_models.Resource.objects.filter(session=session)
+
+    context = {
+        "session": session,
+        "session_notes": session_notes,
+        "action_items": action_items,
+        "resources": resources,
+    }
+
+    return render(request, "coach/session_detail.html", context)
+    
