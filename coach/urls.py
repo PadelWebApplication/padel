71fc2345dc8df7211b655e0cf5384d857aa8e594
kllimenkoo <kliiimenko.al.123654@gmail.com>
2026-04-01 10:47:25 +0300

    action items' and resources' create and update

--- coach/urls.py
+++ coach/urls.py
@@ -16,4 +16,10 @@ urlpatterns = [
      path("add_session_note/<session_id>/", views.add_session_note, name="add_session_note"),
      path("edit_session_note/<session_id>/<session_note_id>/", views.edit_session_note, name="edit_session_note"),
 
+     path("add_action_item/<session_id>/", views.add_action_item, name="add_action_item"),
+     path("edit_action_item/<session_id>/<action_item_id>/", views.edit_action_item, name="edit_action_item"),
+
+     path("add_resource/<session_id>/", views.add_resource, name="add_resource"),
+     path("edit_resource/<session_id>/<resource_id>/", views.edit_resource, name="edit_resource"),
+
 ]
--- coach/views.py
+++ coach/views.py
@@ -36,13 +36,13 @@ def session_detail(request, session_id):
     coach = coach_models.Coach.objects.get(user=request.user)
     session = base_models.Session.objects.get(session_id=session_id, coach=coach)
 
-    session_notes = base_models.SessionNote.objects.filter(session=session)
+    session_note = base_models.SessionNote.objects.get(session=session)
     action_items = base_models.ActionItem.objects.filter(session=session)
     resources = base_models.Resource.objects.filter(session=session)
 
     context = {
         "session": session,
-        "session_notes": session_notes,
+        "session_note": session_note,
         "action_items": action_items,
         "resources": resources,
     }
@@ -121,3 +121,89 @@ def edit_session_note(request, session_id, session_note_id):
         messages.success(request, "Session Note Updated Successfully")
         return redirect("coach:session_detail", session.session_id)
 
+@login_required
+def add_action_item(request, session_id):
+    coach = coach_models.Coach.objects.get(user=request.user)
+    session = base_models.Session.objects.get(session_id=session_id, coach=coach)
+
+    if request.method == "POST":
+        task = request.POST.get("task")
+        description = request.POST.get("description")
+        due_date = request.POST.get("due_date")
+        is_completed = request.POST.get("is_completed") == "on"
+
+        base_models.ActionItem.objects.create(
+            session=session, 
+            task=task, 
+            description=description, 
+            due_date=due_date if due_date else None, 
+            is_completed=is_completed
+        )
+
+        messages.success(request, "Action Item Added Successfully")
+        return redirect("coach:session_detail", session.session_id)
+    
+
+@login_required
+def edit_action_item(request, session_id, action_item_id):
+    coach = coach_models.Coach.objects.get(user=request.user)
+    session = base_models.Session.objects.get(session_id=session_id, coach=coach)
+    action_item = base_models.ActionItem.objects.get(id=action_item_id, session=session)
+
+    if request.method == "POST":
+        task = request.POST.get("task")
+        description = request.POST.get("description")
+        due_date = request.POST.get("due_date")
+        is_completed_raw = request.POST.get("is_completed")
+        is_completed = True if is_completed_raw == "on" else False
+
+        action_item.task = task
+        action_item.description = description
+        action_item.due_date = due_date if due_date else None
+        action_item.is_completed = is_completed
+        action_item.save()
+
+        messages.success(request, "Action Item Updated Successfully")
+        return redirect("coach:session_detail", session.session_id)
+    
+@login_required
+def add_resource(request, session_id):
+    coach = coach_models.Coach.objects.get(user=request.user)
+    session = base_models.Session.objects.get(session_id=session_id, coach=coach)
+
+    if request.method == "POST":
+        title = request.POST.get("title")
+        link = request.POST.get("link")
+        file = request.FILES.get("file")
+        
+        base_models.Resource.objects.create(
+            session=session, 
+            title=title, 
+            link=link, 
+            file=file
+        )
+
+        messages.success(request, "Resource Added Successfully")
+        return redirect("coach:session_detail", session.session_id)
+    
+
+@login_required
+def edit_resource(request, session_id, resource_id):
+    coach = coach_models.Coach.objects.get(user=request.user)
+    session = base_models.Session.objects.get(session_id=session_id, coach=coach)
+    resource = base_models.Resource.objects.get(id=resource_id, session=session)
+
+    if request.method == "POST":
+        title = request.POST.get("title")
+        link = request.POST.get("link")
+        file = request.FILES.get("file")
+        if file:
+            resource.file = file
+
+        resource.title = title
+        resource.link = link
+        resource.file = file
+        resource.save()
+
+        messages.success(request, "Resource Updated Successfully")
+        return redirect("coach:session_detail", session.session_id)
